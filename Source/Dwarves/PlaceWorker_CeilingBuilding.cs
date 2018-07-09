f499df8e2dccfe13c1cb8958af51ffa02a82d8ff
isacmms <isacmms@gmail.com>
2018-07-09 20:48:59 -0300

    Light fixes
    - Created PlaceWorkers for CeilingBuilding (Dwarven Chandelier) and
    WallBuilding (Dwarven Ithildin Wall Light and Wall Sconce).
    - Dwarven Ithildin Wall Light is now rotatable and smaller, so it looks
    better positioned at the edge of the tile.
    - Patched core and elven beds to accept Dwarven End Table and Dwarven
    Dresser as linkables.

---
+++ Source/Dwarves/PlaceWorker_CeilingBuilding.cs
@@ -0,0 +1,28 @@
+﻿using Verse;
+
+namespace Dwarves {
+
+    public class PlaceWorker_CeilingBuilding : PlaceWorker {
+
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, 
+                                                            IntVec3 loc, 
+                                                               Rot4 rot, 
+                                                                Map map, 
+                                                              Thing thingToIgnore = null) {
+
+            if (!map.roofGrid.Roofed(loc))
+                return (AcceptanceReport)"Must be placed under roof.";
+
+            Building building = loc.GetEdifice(map);
+
+            if (building != null && building.def != null && building.def.graphicData != null) {
+                if (building.def.IsDoor)
+                    return "Can't be placed over doors.";
+                if ((building.def.graphicData.linkFlags & (LinkFlags.Wall | LinkFlags.Rock)) != 0)
+                    return "Can't be placed over walls.";
+            }
+
+            return AcceptanceReport.WasAccepted;
+        }
+    }
+}
