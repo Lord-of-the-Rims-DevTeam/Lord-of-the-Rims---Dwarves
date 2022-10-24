1d005b2ee76de18152e18a8ce7afa1ab3e465ecb
Matthew Walls <matt.walls31@gmail.com>
2022-10-24 10:21:25 -0700

    Beards and helmets fix
    Dwarven beards should now show through most helmets

--- Source/HarmonyMisc.cs
+++ Source/HarmonyMisc.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using RimWorld;
 using RimWorld.Planet;
+using UnityEngine;
 using Verse;
+using Quaternion = System.Numerics.Quaternion;
+using Vector3 = System.Numerics.Vector3;
 
 namespace Dwarves
 {
@@ -12,7 +17,37 @@ namespace Dwarves
         public static void HarmonyMisc(Harmony harmony)
         {
             harmony.Patch(AccessTools.Method(typeof(Pawn_GeneTracker), "get_CanHaveBeard"), null,
-                    new HarmonyMethod(typeof(HarmonyDwarves), nameof(get_FemaleDwarvesCanHaveBeards_PostFix)));
+                new HarmonyMethod(typeof(HarmonyDwarves), nameof(get_FemaleDwarvesCanHaveBeards_PostFix)));
+
+            harmony.Patch(AccessTools.Method(typeof(PawnRenderer), name: "DrawHeadHair"),
+                null, null,
+                transpiler: new HarmonyMethod(typeof(HarmonyDwarves), nameof(BeardCheckTranspiler)));
+        }
+
+        //Dwarves should always have their beards visible no matter what
+        public static IEnumerable<CodeInstruction> BeardCheckTranspiler(
+            IEnumerable<CodeInstruction> instructions, ILGenerator il)
+        {
+            List<CodeInstruction> instructionList = instructions.ToList();
+            FieldInfo fullHeadInfo = AccessTools.Field(typeof(BodyPartGroupDefOf), nameof(BodyPartGroupDefOf.FullHead));
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                CodeInstruction instruction = instructionList[i];
+                yield return instruction;
+                if (i > 0 && instructionList[i - 1].OperandIs(fullHeadInfo))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld,
+                        AccessTools.Field(typeof(PawnRenderer), name: "pawn"));
+                    yield return new CodeInstruction(OpCodes.Call,
+                        AccessTools.Method(typeof(HarmonyDwarves), nameof(NotADwarf)));
+                    yield return new CodeInstruction(OpCodes.And);
+                }
+            }
+        }
+        private static bool NotADwarf(Pawn pawn)
+        {
+            return pawn.def.defName != "LotRD_DwarfStandardRace";
         }
 
         //Female dwarves should always be able to grow beards
